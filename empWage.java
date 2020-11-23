b5a6678d4e024d815c80a8657ebbe8386f295b2c
Tejeswar0475 <tejeswar096@gmail.com>
2020-11-23 17:52:39 +0530

    Added check for Employee attendace

--- empWage.java
+++ empWage.java
@@ -1,6 +1,29 @@
-class empWage{
-	public static void main(String[] args)
-	{
-		System.out.println("Welcome to employee wage computation");
-	}
+/* package codechef; // don't place package name! */
+
+import java.util.Random;
+import java.lang.*;
+import java.io.*;
+
+
+class Employee
+{
+
+        public static void main(String[] args)
+        {
+            int head=1;
+            int tail=0;
+
+            System.out.println("Welcome to employee wage computation");
+            Random random=new Random();
+            int  randomNum=random.nextInt(2);
+            if(randomNum == head)
+            {
+                System.out.println("Employee is present:"+randomNum);
+            }
+            else
+            {
+                System.out.println("Employee is absent:"+randomNum);
+            }
+
+        }
 }
