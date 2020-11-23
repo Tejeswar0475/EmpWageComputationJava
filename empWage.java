08f6b0a61a87d0719f973de7bb9fe5fa89fe6f45
Tejeswar0475 <tejeswar096@gmail.com>
2020-11-24 00:06:25 +0530

    Refactor the code for getWorkingHours implementation

--- empWage.java
+++ empWage.java
@@ -7,6 +7,28 @@ import java.io.*;
 class Employee
 {
 
+	int getEmpHours(int randomNumber,int partTimeHour,int fullTimeHour,int wagePerHour,int absent)
+	{
+		int temp=randomNumber;
+		int tempWage=0;
+		switch(temp)
+		{
+		case 1:
+			int partTimeWage=partTimeHour*wagePerHour;
+    			tempWage=partTimeWage;
+    			break;
+    		case 2:
+    			int fullTimeWage=fullTimeHour*wagePerHour;
+    			tempWage=fullTimeWage;
+    			break;
+    		default:
+    			tempWage=absent;
+    		}
+
+		return tempWage;
+	}
+
+
         public static void main(String[] args)
         {
 		int fullTimePresent=2;
@@ -16,24 +38,25 @@ class Employee
 		int fullTimeHour=8;
 		int partTimeHour=4;
 		int dailyEmpWage=0;
+		int workingDayPerMonth=20;
+		int counter=0;
+	        int totalWorkingDays=20;
+	        int tempWage=0;
 
-		System.out.println("Welcome to employee wage computation");
-		Random random=new Random();
-		int randomNum=random.nextInt(3);
-		switch(randomNum)
-		{
-	                case 1:
-               			int partTimeWage=(partTimeHour*wagePerHour);
-	                	System.out.println("Part Time Employee "+partTimeWage);
-	                	break;
-	                case 2:
+		Employee emp=new Employee();
+	        System.out.println("Welcome to employee wage computation");
+		while( counter != workingDayPerMonth )
+	        {
+	        	Random random=new Random();
+	                System.out.println("Day   Wage");
+	                for(int i=1; i<=totalWorkingDays; i++)
+	                {
+	                	int randomNum=random.nextInt(3);
+	                    	int wagesOfEmployee=emp.getEmpHours(randomNum, partTimeHour, fullTimeHour, wagePerHour, absent);
+	                    	System.out.println("Day["+i+"]-"+wagesOfEmployee);
+	                    	counter++;
+	                 }
 
-                		int fullTimeWage=(fullTimeHour*wagePerHour);
-                		System.out.println("Full Time Employee "+fullTimeWage);
-                		break;
-	                default:
-                		System.out.println("Absent "+absent);
-		}
-
-        }
+        	}
+	}
 }
